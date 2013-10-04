f3f2d32fbc00639de4c921e9b9089bc3621488aa
Daniel Hough <daniel.hough@huddle.com>
2013-10-04 16:58:46 +0100

    Accepts afterResponse to change the API when something happens

--- server.js
+++ server.js
@@ -12,29 +12,52 @@ app.configure(function(){
 	app.use(app.router);
 });
 
-function createInvalidDataException() {
-	return new Exception('You have to provide a JSON object with the following structure: \n' + JSON.stringify({ request : { method : '[GET|PUT|POST|DELETE]', url : '(relative URL e.g. /hello)' }, response : { code : '(HTTP Response code e.g. 200/400/500)', body : '(a JSON object)' } } ));
+function createInvalidDataException(data) {
+	return new Error('You have to provide a JSON object with the following structure: \n' + JSON.stringify({ request : { method : '[GET|PUT|POST|DELETE]', url : '(relative URL e.g. /hello)' }, response : { code : '(HTTP Response code e.g. 200/400/500)', body : '(a JSON object)' } }, null, 4) + ' but you provided: \n' + JSON.stringify(data, null, 4));
 }
 
-function createEndpoint(data) {
-	var specifiedRequest, specifiedResponse;
+function clearRouteForRequest(request) {
+	var i,j;
+	if (!app.routes[request.method]) return;
+	for (i = app.routes[request.method].length - 1; i >= 0; i--) {
+		if (app.routes[request.method][i].path === request.url) {
+			console.log('Clearing existing route at ' + request.method + ': ' + request.url + ' (1/2)');
+			app.routes[request.method].shift(i);
+			break;
+		}
+	}
+}
+
+function createRoute(data) {
+	var specifiedRequest, specifiedResponse, afterSpecifiedResponse;
 	if (!data.request || !data.request.method || !data.request.url || !data.response || !data.response.code || !data.response.body) {
-		throw createInvalidDataException();
+		throw createInvalidDataException(data);
 	}
 
 	console.log('Setting up ' + data.request.method + ' ' + data.request.url + ' to return ' + data.response.code + ' with a body of length ' + JSON.stringify(data.response.body).length);
+	console.log('ROUTER obj: \n' + JSON.stringify(app._router.map));
 
 	specifiedRequest = data.request;
 	specifiedResponse = data.response;
+	afterSpecifiedResponse = data.afterResponse;
+
+	clearRouteForRequest(specifiedRequest);
 
 	app[specifiedRequest.method](specifiedRequest.url, function (req, res) {
+		console.log('Request to ' + specifiedRequest.url);
 		res.send(specifiedResponse.code, specifiedResponse.body);
+
+		if (afterSpecifiedResponse && afterSpecifiedResponse.endpoints) {
+			afterSpecifiedResponse.endpoints.forEach(function (endpoint) {
+				createRoute(endpoint);
+			});
+		}
 	});
 }
 
 app.post('/_request', function(req, res){
 	try {
-		createEndpoint(req.body);
+		createRoute(req.body);
 		res.send(200, { done : true });
 	} catch (e) {
 		console.log('Error: ', e);
@@ -46,7 +69,7 @@ if (program.file) {
 	file = require(program.file);
 
 	file.forEach(function (endpoint) {
-		createEndpoint(endpoint);
+		createRoute(endpoint);
 	});
 }
 
