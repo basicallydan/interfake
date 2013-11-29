65e1b633c012e062ec0a3dd9f1e4644f0c5c1ad8
Dan Hough <daniel.hough@gmail.com>
2013-11-29 16:06:09 +0000

    Attempting to get it ready for NPM. Probably gonna screw this up a few times

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+
+var program = require('commander');
+var interfake = require('./lib/server');
+program
+	.version('0.0.1')
+	.option('-f, --file [file]', 'Load an API from a JSON file [file]')
+	.option('-p --port [port]', 'Specify a port for Interfake to listen on', 3000)
+	.parse(process.argv);
+var file;
+
+if (program.file) {
+	interfake.loadFile(program.file);
+}
+
+interfake.listen(program.port);
