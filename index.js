a44fdc0b28b3a2f137118c2a61382fe143f1e063
Dan Hough <daniel.hough@gmail.com>
2014-05-24 11:31:06 +0100

    Basic support for fluent command line interface for #5

--- index.js
+++ index.js
@@ -3,12 +3,17 @@
 var program = require('commander');
 var Interfake = require('./lib/server');
 var packageInfo = require('./package.json');
+var fs = require('fs');
+var PEG = require('pegjs');
+var grammar, parser, endpoints;
 program
 	.version(packageInfo.version)
 	.option('-f, --file [file]', 'Load an API from a JSON file [file]')
 	.option('-p --port [port]', 'Specify a port for Interfake to listen on', 3000)
 	.option('-d --debug', 'Debug mode, turns on console messages')
 	.parse(process.argv);
+
+// console.log(program.args[0]);
 
 var opts = {
 	debug: false
@@ -22,6 +27,25 @@ var interfake = new Interfake(opts);
 
 if (program.file) {
 	interfake.loadFile(program.file);
+}
+
+function addFluentEndpoint(endpoint) {
+	var e = interfake[endpoint.method](endpoint.path).status(endpoint.status);
+	if (endpoint.creates) {
+		endpoint.creates.forEach(function (c) {
+			e.creates[c.method](c.path).status(c.status);
+		});
+	}
+}
+
+if (['get', 'put', 'post', 'del'].indexOf(program.args[0]) !== -1) {
+	// Fluent command line mode
+	// console.log('Fluent command line mode!');
+	// This should be refined using http://pegjs.majda.cz/online
+	grammar = fs.readFileSync('./lib/grammar.pegjs') + '';
+	parser = PEG.buildParser(grammar);
+	endpoints = parser.parse(program.args.join(' '));
+	endpoints.forEach(addFluentEndpoint);
 }
 
 interfake.listen(program.port);
