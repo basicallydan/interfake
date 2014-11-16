72fb058efe47a96b050662da2545d4c6e15f85e1
Dan Hough <daniel.hough@gmail.com>
2014-11-16 18:32:31 +0000

    Closes #27, can now watch files on the command line

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ program
 	.option('-f, --file [file]', 'Load an API from a JSON file [file]')
 	.option('-p --port [port]', 'Specify a port for Interfake to listen on', 3000)
 	.option('-d --debug', 'Debug mode, turns on console messages')
+	.option('-w --watch', 'If a loaded file changes, it will be re-loaded')
 	.parse(process.argv);
 
 // console.log(program.args[0]);
@@ -20,10 +21,14 @@ if (program.debug) {
 	opts.debug = true;
 }
 
+if (program.watch) {
+	opts.watch = true;
+}
+
 var interfake = new Interfake(opts);
 
 if (program.file) {
-	interfake.loadFile(program.file);
+	interfake.loadFile(program.file, opts);
 }
 
 interfake.listen(program.port);
