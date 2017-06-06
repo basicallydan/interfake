6a18c3a49b986eb7b06d27726343e35f0eb89e8d
ap <ap@norselab.com>
2017-06-06 10:22:11 -0300

    feat(middlewares): add ability to pass custom middlewares to application
    - added .idea folder to .gitignore
    - updated npm-shrinkwrap output

--- index.js
+++ index.js
@@ -3,12 +3,15 @@
 var program = require('commander');
 var Interfake = require('./lib/server');
 var packageInfo = require('./package.json');
+var path = require('path');
+
 program
 	.version(packageInfo.version)
 	.option('-f, --file [file]', 'Load an API from a JSON file [file]')
 	.option('-p --port [port]', 'Specify a port for Interfake to listen on', 3000)
 	.option('-d --debug', 'Debug mode, turns on console messages')
 	.option('-w --watch', 'If a loaded file changes, it will be re-loaded')
+	.option('-m --middlewares', 'Paths to middleware files')
 	.parse(process.argv);
 
 // console.log(program.args[0]);
@@ -23,6 +26,18 @@ if (program.debug) {
 
 if (program.watch) {
 	opts.watch = true;
+}
+
+if (program.middlewares) {
+	var middlewares = program.middlewares.map(function (middleware) {
+		console.log('Loading middleware:', middleware);
+
+		return require(path.resolve(middleware));
+	});
+
+	if (middlewares.length) {
+        opts.middlewares = middlewares;
+	}
 }
 
 var interfake = new Interfake(opts);
